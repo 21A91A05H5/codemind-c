fad42af7c0f9238fc18a8a19e4629c61d88ae977
21A91A05H5 <97424513+21A91A05H5@users.noreply.github.com>
2022-04-18 21:42:48 +0530

    #include<stdio.h> int main() {     int n,sum=0,d,k,temp;     scanf("%d",&n);     temp=n*n;     while(n)     {         d=n%10;         sum=(sum*10)+d;         n=n/10;     }     k=sum*sum;     sum=0;     while(k)     {         d=k%10;         sum=(sum*10)+d;         k=k/10;     }     if(temp==sum)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,27 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,temp,r=0,d,k;
+    int n,sum=0,d,k,temp;
     scanf("%d",&n);
     temp=n*n;
     while(n)
     {
         d=n%10;
-        r=(r*10)+d;
+        sum=(sum*10)+d;
         n=n/10;
     }
-    k=r*r;
-    r=0;
+    k=sum*sum;
+    sum=0;
     while(k)
     {
         d=k%10;
-        r=(r*10)+d;
+        sum=(sum*10)+d;
         k=k/10;
     }
-    if(temp==r)
+    if(temp==sum)
     {
         printf("True");
-        return 0;
     }
     else
     {
