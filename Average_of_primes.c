931b3c08206a8903010b15c67b17c352c589c47d
21A91A05H5 <97424513+21A91A05H5@users.noreply.github.com>
2022-04-28 21:28:37 +0530

    #include<stdio.h> #include<math.h> int main() { 	int n,arr[100],i,c=0,j,d=0; 	float avg,sum=0; 	scanf("%d",&n); 	for(i=0;i<=n;i++) 	{ 		scanf("%d",&arr[i]); 	}
    		for(i=0;i<=n;i++)
    	{
    	    c=0;
    	    for(j=1;j<=arr[i];j++)
    	    {
        	    if(arr[i]%j==0)
        	    {
        	       
        	       c++;
        	    }
    	    }
    	    if(c==2)
    	    {
    	        sum=sum+arr[i];
    	    }
    	    if(c==2)
    	    {
    	        d++;
    	    }
    	}
    	 avg=sum/d;
    	printf("%.2f",avg);
    	
    }

---
+++ Average_of_primes.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,arr[100],i,c=0,j,d=0;
+	float avg,sum=0;
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	    
+		for(i=0;i<=n;i++)
+	{
+	    c=0;
+	    for(j=1;j<=arr[i];j++)
+	    {
+    	    if(arr[i]%j==0)
+    	    {
+    	       
+    	       c++;
+    	    }
+	    }
+	    if(c==2)
+	    {
+	        sum=sum+arr[i];
+	    }
+	    if(c==2)
+	    {
+	        d++;
+	    }
+	}
+	 avg=sum/d;
+	printf("%.2f",avg);
+	
+}
