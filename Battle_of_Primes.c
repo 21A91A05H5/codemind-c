091065d7c8d80fe2a724f26ac2616e0f83b66573
Sakethram_421 <97424513+21A91A05H5@users.noreply.github.com>
2022-07-01 22:58:47 +0530

    #include<stdio.h> int isprime(int s2) {     int i;     for(i=2;i<s2;i++)     {         if(s2%i==0)         {             return 0;         }
        }
        return 1;
    }
    int fun(int s1)
    {
        int i,s2;
        for(i=1;i<=10;i++)
        {
            s2=s1+i;
            if(isprime(s2))
            {
                return i;
            }
        }
    }
    int main()
    {
        int a,b,i,s1,s2,k=0;
        scanf("%d%d",&a,&b);
        s1=a+b;
        k=fun(s1);
        printf("%d",k);
    }

---
+++ Battle_of_Primes.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int isprime(int s2)
+{
+    int i;
+    for(i=2;i<s2;i++)
+    {
+        if(s2%i==0)
+        {
+            return 0;
+        }
+
+    }
+    return 1;
+}
+int fun(int s1)
+{
+    int i,s2;
+    for(i=1;i<=10;i++)
+    {
+        s2=s1+i;
+        if(isprime(s2))
+        {
+            return i;
+        }
+    }
+}
+int main()
+{
+    int a,b,i,s1,s2,k=0;
+    scanf("%d%d",&a,&b);
+    s1=a+b;
+    k=fun(s1);
+    printf("%d",k);
+}
